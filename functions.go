ad0915f80aec10efdc58b1a1fb37dbc23e245914
Audrius Kniuras <audrius.kniuras@tesonet.com>
2021-06-26 14:07:04 +0300

    moore

--- functions.go
+++ functions.go
@@ -1,5 +1,29 @@
 package main
 
-func functions() {
+import "fmt"
 
+// if few parameters are of the same type, list them and then tell the type
+func say(greeting, name string) {
+	fmt.Printf("%v %v - multiple parameters, same type\n", greeting, name)
+}
+
+func pntr(number *int) {
+	*number = 30
+}
+
+func variadic_func(values ...int) {
+	result := 0
+	for _, v := range values {
+		result += v
+	}
+	fmt.Printf("%v - variadic parameters", result)
+}
+
+func functions() {
+	say("Hello", "Audrius")
+	a := 20
+	pntr(&a)
+	fmt.Printf("%v - change value in a function\n", a)
+
+	variadic_func(1, 2, 3, 4, 5)
 }
